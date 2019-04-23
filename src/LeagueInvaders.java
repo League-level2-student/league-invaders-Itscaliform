a4ebe8f8940548522deac20f60c9185c5fff3894
71856 <71856@0338S-S-R0B6ZHB.sdcs.local>
2019-04-23 14:18:45 -0700

    GAMEPANEL on Game loop Section 5.

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,34 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	GamePanel panel;
+	static final int WIDTH=500;
+	static final int HEIGHT=800;
+	
+	LeagueInvaders(){
+		frame= new JFrame();
+		panel = new GamePanel();
+	}
+	
+	
+	
+	
+	void setup() { 
+		frame.add(panel);
+		frame.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		
+		frame.setVisible(true);
+		
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+public static void main(String[] args) {
+	LeagueInvaders league = new LeagueInvaders();
+	league.setup();
+}
+
 
 }
